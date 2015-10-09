67a1df351cb735039bf170af662bea39adffb936
Per Solberg <persolberg@pso-mba.local>
2015-10-09 15:47:41 +0200

    Initial Commit

---
+++ mySHiT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mySHiT
+//
+//  Created by Per Solberg on 2015-10-09.
+//  Copyright © 2015 Per Solberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
