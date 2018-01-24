af267bf194e4fdc31e94a5c31b8d1b8f69f6e47f
persolberg <persolberg@hotmail.com>
2018-01-25 00:33:56 +0100

    Version 3.1: Additional files

---
+++ mySHiT/UIViewController+mySHiT.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+mySHiT.swift
+//  mySHiT
+//
+//  Created by Per Solberg on 2018-01-24.
+//  Copyright © 2018 &More AS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController
+{
+    func isSame(_ vc:UIViewController) -> Bool {
+        if type(of:vc) != type(of:self) {
+            return false
+        } else {
+            print("Not sure if view controllers are identical - probably not")
+            return false
+        }
+    }
+}
